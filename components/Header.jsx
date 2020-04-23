9530b1d3125b959abc97f6320479345de9008523
Aydoğan Sarı <aydogansari@hotmail.com>
2020-04-23 11:57:40 +0300

    Responsive iyileştirmeleri yapıldı.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Nav from "./Nav";
 import HamburgerMenuComp from "./HamburgerMenuComp";
+import Link from "./Link";
 
 const Header = () => {
   return (
     <header>
-      <h4 id="logo">
-        <img src="./logo.png" />
-        FAVORİFİLM
-      </h4>
+      <Link href="/">
+        <h4 id="logo">
+          <img src="./logo.png" />
+          FAVORİFİLM
+        </h4>
+      </Link>
       <Nav id="menus" />
       <HamburgerMenuComp />
     </header>
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -18,6 +18,11 @@ const Layout = ({ children }) => {
     <React.Fragment>
       <Head>
         <title>FAVORİFİLM</title>
+        <meta charset="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
       </Head>
       <Header />
       <MobileMenu />
